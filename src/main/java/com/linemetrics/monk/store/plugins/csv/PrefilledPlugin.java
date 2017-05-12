722c33d6be2dea74a95b79afd91ae5ac513507f4
Klemens Engelbrechtsmüller <K.Engelbrechtsmueller@linemetrics.com>
2017-05-12 11:03:19 +0200

    Allow item configuration on prefilled-plugin

--- src/main/java/com/linemetrics/monk/store/plugins/csv/PrefilledPlugin.java
+++ src/main/java/com/linemetrics/monk/store/plugins/csv/PrefilledPlugin.java
@@ -237,7 +237,7 @@ public class PrefilledPlugin implements IStore {
                                 numberLocale,
                                 mi,
                                 ctx,
-                                null
+                                item
                             );
 
         File exportFile = new File(
